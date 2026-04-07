6f6e7ab5d6c3736e321f6497ea52508fb957cd5f
Harsh <harshbajaj544@gmail.com>
2026-04-07 14:50:13 +0530

    Feature: implement child_process orchestration and absolute path deployment.

--- workspace/controllers/projectController.js
+++ workspace/controllers/projectController.js
@@ -1,4 +1,6 @@
 import crypto from "crypto";
+import path from "path";
+import { spawn } from "child_process";
 
 let projects = [];
 
@@ -7,9 +9,9 @@ export const getAllProjects = (req, res) => {
 };
 
 export const createProject = (req, res) => {
-  const { name, repoURL } = req.body;
-  if (!name || !repoURL) {
-    const error = new Error("Name and repoURL are required.");
+  const { name, path, port, env, repoURL } = req.body;
+  if (!name || !path || !port) {
+    const error = new Error("Name, Path, and Port are required");
     error.statusCode = 400;
     throw error;
   }
@@ -17,6 +19,9 @@ export const createProject = (req, res) => {
     id: crypto.randomUUID(),
     name,
     repoURL,
+    port,
+    path,
+    env: env || {},
     status: "idle",
     lastDeployed: null,
   };
@@ -33,11 +38,48 @@ export const deployProject = (req, res) => {
     throw error;
   }
   project.status = "deploying";
-  project.lastDeployed = new Date().toISOString();
+  res.json({
+    message: `Deployment started for project ${project.name}.`,
+    project,
+  });
 
-  setTimeout(() => {
-    project.status = "Success";
-    project.lastDeployed = new Date().toISOString();
-    console.log(`log ${project.name} deployed`);
-  }, 5000);
+  const scriptPath = path.join(process.cwd(), "..", "deploy.sh");
+
+  console.log(`[DEBUG] Attempting to run: bash ${scriptPath}`);
+
+  const envVars = Object.entries(project.env || {})
+    .map(([key, value]) => `-e ${key}=${value}`)
+    .join(" ");
+
+  const child = spawn("bash", [
+    scriptPath,
+    project.name,
+    project.port,
+    project.path,
+    envVars
+  ]);
+
+  child.stdout.on("data", (data) => {
+    console.log(`[STDOUT - ${project.name}]: ${data}`);
+  });
+  child.stderr.on("data", (data) => {
+    console.error(`[STDERR - ${project.name}]: ${data}`);
+  });
+
+  child.on("close", (code) => {
+    if (code === 0) {
+      project.status = "success";
+      project.lastDeployed = new Date().toISOString();
+      console.log(
+        `>>> Success ${project.name} is live on port ${project.port}`,
+      );
+    } else {
+      project.status = "failed";
+      console.error(
+        `>>> ERROR: ${project.name} deployment failed with code ${code}`,
+      );
+    }
+  });
 };
+
+export const getProjects = (req, res) => res.json(projects);
